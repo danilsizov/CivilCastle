c5487e68378187c9a1d339610b98c765634835b4
danilsizov <danilsizov3@gmail.com>
2016-07-16 18:16:45 +0300

    Coins

--- app/src/main/java/com/example/goo/civilcastle/MainActivity.java
+++ app/src/main/java/com/example/goo/civilcastle/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.goo.civilcastle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void myMainClick(View view) {
+
+
+
     }
 }
