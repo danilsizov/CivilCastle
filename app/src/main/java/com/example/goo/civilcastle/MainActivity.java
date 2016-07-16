27cbf0a156c197ffef57c32ebf1ddd51ad530afb
danilsizov <danilsizov3@gmail.com>
2016-07-16 19:18:45 +0300

    Start developed of game logick

--- app/src/main/java/com/example/goo/civilcastle/MainActivity.java
+++ app/src/main/java/com/example/goo/civilcastle/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.goo.civilcastle;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int numberOfCoins=0;
+    private static final int LENGTH_SHORT = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -17,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void myMainClick(View view) {
 
-
+        TextView myCoinsScore = (TextView)findViewById(R.id.myCoinsScore);
+        numberOfCoins++;
+        myCoinsScore.setText(numberOfCoins + "");
+        Toast.makeText(this, "+1", Toast.LENGTH_SHORT).show();
 
     }
 }
